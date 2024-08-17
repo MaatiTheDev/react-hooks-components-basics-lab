8b5af53f151c2c9b9971ee609cf64aec61d868ca
felix maati <maatifelix001@gmail.com>
2024-08-17 14:15:39 +0300

    final commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+      <p>This section contains information about me and my work.</p>
+    </div>
+  );
+}
+
+export default About;
